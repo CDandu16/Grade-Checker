e5ca17b528ede1c72998e5e5c346904269d6aa18
Chaitanya Dandu <Chaitu@ACA80181.ipt.aol.com>
2015-06-26 19:46:16 -0700

    Initial Commit

---
+++ Grade-Checker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Grade-Checker
+//
+//  Created by Chaitanya Dandu on 6/26/15.
+//  Copyright (c) 2015 Chaitanya Dandu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
